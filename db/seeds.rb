07cc88b5a7cd45dec01b2f9b2ab7a2acbe454de8
Atyun <atyun@Atyuns-Mac-mini.local>
2016-10-13 11:37:18 +0800

    third commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,5 +41,5 @@ Product.create(:title => 'Rails Test Prescriptions',
         procedures for Rails 2 and Rails 3, and introduces popular add-ons,
         including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
       </p>},
-               :image_url => '/images/rtp.jpg',
+               :image_url => '/public/images/rtp.jpg',
                :price => 43.75)
