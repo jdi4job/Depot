3675edecc42b9d29341ebcc9997fed6bdef7fa7d
Atyun <atyun@Atyuns-Mac-mini.local>
2016-10-13 19:55:24 +0800

    forth commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,18 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'line items present')
+        return false
+      end
+    end
 
 end
